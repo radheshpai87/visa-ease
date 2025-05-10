ac7b7ed3e9796b0c991771d57a746504e1b1452b
Radhesh Pai <radhesh.ratnakar.s87@kalvium.community>
2025-05-10 13:01:43 +0530

    added backend

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/visa_system/', // 👈 This is important for GitHub Pages
   plugins: [
     tailwindcss(),
     react()
